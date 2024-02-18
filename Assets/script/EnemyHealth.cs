42335a8dfc646697d98a038716ac558ae9790537
Motpixel <mn5896057@gmail.com>
2024-02-18 16:15:29 +0300

    a03

---
+++ Assets/script/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float value = 100;
+
+    private void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/script/Fireball.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float damage = 10;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Invoke("DestroyFireball", lifetime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyFireball();          
+    }
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.value -= damage;
+            if(enemyHealth.value <= 0)
+            {
+                Destroy(enemyHealth.gameObject);
+            }
+            
+
+        }
+    }
+
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+}
